ad6afa95c164560425f23b31cda7d00f6de8c3a6
hyun907 <seunghyun020907@naver.com>
2024-09-17 23:55:14 +0900

    refactor: properties 타입 정의

--- src/api/types.ts
+++ src/api/types.ts
@@ -3,18 +3,50 @@ export interface Article {
   title: string;
   createdAt: Date;
   thumbnailUrl: string;
-  properties: any;
+  properties: PageProperties;
 }
+
 export interface MultiSelectOption {
   id: string;
   name: string;
   color: string;
 }
 
+export interface SelectOption {
+  id: string;
+  name: string;
+  color: string;
+}
+
 export interface PageProperties {
-  views: {
+  role?: {
+    id: string;
+    type: "multi_select";
+    multi_select: MultiSelectOption[];
+  };
+  exposure?: {
+    id: string;
+    type: "checkbox";
+    checkbox: boolean;
+  };
+  writer?: {
+    id: string;
+    type: "select";
+    select: SelectOption | null;
+  };
+  status?: {
+    id: string;
+    type: "status";
+    status: SelectOption | null;
+  };
+  views?: {
     id: string;
     type: "number";
     number: number | null;
+  };
+  name?: {
+    id: string;
+    type: "title";
+    title: { type: "text"; text: { content: string; link: null } }[];
   };
 }
