830df71aebc09bb60a9d36332db7695e63279c27
hyun907 <seunghyun020907@naver.com>
2024-09-23 23:09:26 +0900

    style: css 추가 및 스타일링

--- src/app/search/result/page.tsx
+++ src/app/search/result/page.tsx
@@ -1,15 +1,23 @@
 import { Heading, Text, Flex, Card, Inset, Box } from "@radix-ui/themes";
+import { ExclamationTriangleIcon, QuestionMarkCircledIcon } from "@radix-ui/react-icons";
 import { searchArticle } from "@/api/notion";
 import { getFormatDate } from "@/utils/getFormatDate";
 import { Article } from "@/api/types";
 import Link from "next/link";
 import Image from "next/image";
+import styles from "./SearchResultsPage.module.css";
 
 export default async function SearchResultsPage({ searchParams }: { searchParams: { q: string } }) {
   const query = searchParams.q;
 
   if (!query) {
-    return <Text>검색어를 입력해주세요.</Text>;
+    return (
+      <Flex direction={"column"} align={"center"} gap={"6"}>
+        <Heading>검색어를 입력하지 않으셨군요!</Heading>
+        <ExclamationTriangleIcon color="green" width="100" height="100" />
+        <Text color="green">검색어를 입력해주세요.</Text>
+      </Flex>
+    );
   }
 
   let results: Article[] = [];
@@ -21,14 +29,23 @@ export default async function SearchResultsPage({ searchParams }: { searchParams
   }
 
   return (
-    <Flex direction="column" gap="4">
-      <Heading size="6">'{query}'에 대한 검색 결과</Heading>
+    <Flex direction="column" gap="6" p="6">
       {results.length > 0 ? (
-        <Flex direction="row" gap="4" wrap="wrap">
+        <Flex direction="row" gap="6" wrap="wrap" justify="start">
+          <Heading size="6" mb="4">
+            '{query}'에 대한 검색 결과
+          </Heading>
           {results.map(result => (
-            <Link key={result.pageId} href={`/posts/${result.pageId}`} passHref>
+            <Link
+              key={result.pageId}
+              href={`/posts/${result.pageId}`}
+              passHref
+              style={{
+                textDecoration: "none"
+              }}
+            >
               <Box width="240px">
-                <Card size="2">
+                <Card className={styles.card} size="2">
                   <Inset clip="padding-box" side="top" pb="current">
                     <Image
                       src={result.thumbnailUrl || ""}
@@ -39,15 +56,23 @@ export default async function SearchResultsPage({ searchParams }: { searchParams
                       priority
                     />
                   </Inset>
-                  <Heading size="3">{result.title}</Heading>
-                  <Text size="3">{getFormatDate(result.createdAt)}</Text>
+                  <Heading size="4" mb="2" weight="bold" color="gray">
+                    {result.title}
+                  </Heading>
+                  <Text size="1" color="gray">
+                    {getFormatDate(result.createdAt)}
+                  </Text>
                 </Card>
               </Box>
             </Link>
           ))}
         </Flex>
       ) : (
-        <Text>검색 결과가 없습니다.</Text>
+        <Flex direction={"column"} align={"center"} gap={"6"}>
+          <QuestionMarkCircledIcon color="green" width="100" height="100" />
+          <Heading>{query}에 대한 검색 결과가 없어요!</Heading>
+          <Text color="green">다른 검색어로 다시 검색해 보세요.</Text>
+        </Flex>
       )}
     </Flex>
   );
